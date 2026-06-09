0cc0086e7f3020a729cf7dd5bb1a81224aae4c38
Sahana0430 <sahanashetty9955@gmail.com>
2026-06-09 15:28:27 +0530

    Update build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+pipeline {
+agent any
+stages {
+stage('Clone Repository') {
+steps {
+git branch: 'main',
+url: 
+}
+stage('Status') {
+steps {
+echo "Sucessfully fetched the repo"'https://github.com/your-username/your-repo.git'
+}
+
+}
+}
